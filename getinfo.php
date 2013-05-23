7f39c063bbdd7c7b2da688ad04b46b35d303ace3
myothein <mr.myothein@gmail.com>
2013-05-23 16:16:53 +0800

    Update getinfo.php

--- getinfo.php
+++ getinfo.php
@@ -1,1 +1,15 @@
-getinfo
+<?php
+require_once("data.php");
+
+$ArrayURL = split('/', $_SERVER["REQUEST_URI"]); 
+$id = $ArrayURL[1];
+
+$data = new baseObj();
+// set table name
+$data->table = "Property";
+if (is_object($data) == true) $status = '200 OK';
+$status_header = 'HTTP/1.1 '.$status;
+
+header($status_header);
+echo json_encode( $data->getAll($id) );
+?>
