ea666ca7c936608c58c046a3bb865889a84f761c
myothein <mr.myothein@gmail.com>
2013-05-24 14:29:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php
-include "common.php";
-
-// task 1-1
-echo "<h1> Task1-1</h1>";
-echo "Output : ";
-echo FizzBuzz(12,16);
-
-echo "<hr>";
-
-// task 1-2
-echo "<h1> Task1-2</h1>";
-echo "Output : ";
-echo FizzBuzzBazz(4,11);
-?>
+index
