208b2b7ce48de730fef547a20d514e9e724bfbf0
myothein <mr.myothein@gmail.com>
2013-05-24 14:22:15 +0800

    Update task1.php

--- task1.php
+++ task1.php
@@ -1,15 +1,17 @@
 <?php
 include "common.php";
 
+$n = new Number();
+
 // task 1-1
 echo "<h1> Task1-1</h1>";
 echo "Output : ";
-echo FizzBuzz(12,16);
+echo $n->getFizzBuzz(12,16);
 
 echo "<hr>";
 
 // task 1-2
 echo "<h1> Task1-2</h1>";
 echo "Output : ";
-echo FizzBuzzBazz(4,11);
+echo $n->getFizzBuzzBazz(4,11);
 ?>
